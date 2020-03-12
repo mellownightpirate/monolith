ab80accac5e5ab9dc972f1a70b04715a12eb5bbe
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-12 09:48:44 +0000

    express appp

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello there');
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const PORT = 5000
+
+app.get('/api', (req,res) => {
+    res.json({ message: 'The api is UP' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
