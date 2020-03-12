3a8848548ff186193ee417b6eb9de77800564411
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-12 11:38:38 +0000

    hitting api using cra

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getBaseURL() {
+  return process.env.NODE_ENV === 'development'
+  ? 'http://localhost:5000'
+  : ''
+}
+
 function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+fetch(getBaseURL() + '/api')
+.then(res => res.json())
+.then(data => {
+  setMessage(data.message)
+})
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    { message }
       </header>
     </div>
   );
