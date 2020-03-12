cf2ce17079556b3adda55ee312db9c9f0cfa4c5f
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-12 10:01:08 +0000

    set up port that connects to heroku otherwise local port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/api', (req,res) => {
     res.json({ message: 'The api is UP' })
