10f5d65a5d1c3cf19e476711ddd22502e3271f90
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-12 10:45:50 +0000

    setup dotenv

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 
@@ -6,7 +8,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9000
+
+console.log(process.env.LADY);
 
 app.get('/api', (req,res) => {
     res.json({ message: 'The api is UP' })
